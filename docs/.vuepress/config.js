ccd613cf3dc664201d57cd94d71afb20ff557104
HuangQing <mr_hqing@163.com>
2022-03-19 00:23:23 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,25 @@
+
+module.exports = {
+	title: ' ',
+	description: '分享学习的过程和成果~',
+	//主题配置
+	themeConfig: {
+		//Logo
+		logo: '/images/logo-header.png',
+		//最后更新时间
+		lastUpdated: 'Last Updated',
+		nav: [
+			{ text: 'Home', link: '/' },
+			{ text: 'GitHub', link: 'https://github.com/DQIT' },
+			{
+				text: '其他',
+				items: [
+					{ text: 'xxx1', link: '' },
+					{ text: 'xxx2', link: '' }
+				]
+
+			}
+		],
+		sidebar: 'auto'
+	}
+}
---
+++ docs/README.md
@@ -0,0 +1,14 @@
+---
+home: true
+heroImage: /images/logo.png
+actionText: 开始
+actionLink: /guide/GuideHome
+features:
+- title: 后端
+  details: 分享一些Java领域的后端知识
+- title: 前端
+  details: 补充少量的前端知识
+- title: 其他
+  details: 还有运维、数据库、等等
+footer: MIT Licensed | Copyright © 2022-present Evan You
+---
